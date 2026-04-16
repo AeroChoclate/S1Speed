0b0452d194730e35439e42e3fc0c8624355766b4
AeroChoclate <ashtoncardiff20@gmail.com>
2026-04-16 13:35:03 +0100

    Added Config & Keybind
    Added a config so the user can now change the keybind and have it save for every time they play the game with the mod this also hot reloads the config so no need to restart every keybind change.

--- S1Speed/Core.cs
+++ S1Speed/Core.cs
@@ -1,7 +1,9 @@
 ﻿using MelonLoader;
 using S1Speed.Utils;
+using System;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.UI;
 
 [assembly: MelonInfo(typeof(S1Speed.Core), Constants.MOD_NAME, Constants.MOD_VERSION, Constants.MOD_AUTHOR)]
 [assembly: MelonGame(Constants.Game.GAME_STUDIO, Constants.Game.GAME_NAME)]
@@ -17,8 +19,29 @@ namespace S1Speed
 
         private const float SPEED = 5f;
 
+        private MelonPreferences_Category configCategory;
+        private MelonPreferences_Entry<string> keybindEntry;
+
+        private KeyCode toggleKey = KeyCode.Insert;
+
+        private GameObject menuObject;
+        private bool menuVisible;
+
+        private bool waitingForKey;
+
+        private Text speedText;
+        private Text keybindText;
+
         public override void OnInitializeMelon()
         {
+            configCategory = MelonPreferences.CreateCategory("S1Speed");
+            keybindEntry = configCategory.CreateEntry("ToggleKey", "Insert");
+
+            if (Enum.TryParse(keybindEntry.Value, out KeyCode parsedKey))
+            {
+                toggleKey = parsedKey;
+            }
+
             playerMovementType = Type.GetType("ScheduleOne.PlayerScripts.PlayerMovement, Assembly-CSharp");
 
             if (playerMovementType != null)
@@ -27,23 +50,27 @@ namespace S1Speed
                     BindingFlags.Public | BindingFlags.Static);
             }
 
-            LoggerInstance.Msg("Speed mod initialized safely.");
+            LoggerInstance.Msg($"Initialized. Keybind: {toggleKey}");
         }
 
         public override void OnUpdate()
         {
-            HandleInput();
+            HandleMenuToggle();
+            HandleKeybindChange();
+
+            ApplySpeed();
         }
 
-        private void HandleInput()
+        private void HandleMenuToggle()
         {
-            if (Input.GetKeyDown(KeyCode.F6))
+            if (Input.GetKeyDown(toggleKey) && !waitingForKey)
             {
-                speedEnabled = !speedEnabled;
+                menuVisible = !menuVisible;
 
-                ApplySpeed();
+                if (menuObject == null)
+                    CreateMenu();
 
-                LoggerInstance.Msg($"Speed: {(speedEnabled ? "ON" : "OFF")}");
+                menuObject.SetActive(menuVisible);
             }
         }
 
@@ -59,6 +86,111 @@ namespace S1Speed
             {
                 LoggerInstance.Error("Speed error: " + ex.Message);
             }
+        }
+
+        private void HandleKeybindChange()
+        {
+            if (!waitingForKey) return;
+
+            foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    toggleKey = key;
+                    keybindEntry.Value = key.ToString();
+                    keybindEntry.Save();
+
+                    waitingForKey = false;
+
+                    LoggerInstance.Msg($"New keybind: {toggleKey}");
+                    UpdateButtonText();
+
+                    break;
+                }
+            }
+        }
+
+        private void CreateMenu()
+        {
+            menuObject = new GameObject("S1SpeedMenu");
+
+            var canvas = menuObject.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+            menuObject.AddComponent<CanvasScaler>();
+            menuObject.AddComponent<GraphicRaycaster>();
+
+            GameObject panel = new GameObject("Panel");
+            panel.transform.SetParent(menuObject.transform, false);
+
+            var img = panel.AddComponent<Image>();
+            img.color = new Color(0, 0, 0, 0.75f);
+
+            RectTransform rect = panel.GetComponent<RectTransform>();
+            rect.sizeDelta = new Vector2(300, 200);
+            rect.anchoredPosition = new Vector2(200, -150);
+
+            GameObject speedBtn = CreateButton(panel, new Vector2(0, 40));
+            speedText = CreateText(speedBtn, "Speed: OFF");
+
+            speedBtn.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                speedEnabled = !speedEnabled;
+                UpdateButtonText();
+            });
+
+            GameObject keyBtn = CreateButton(panel, new Vector2(0, -40));
+            keybindText = CreateText(keyBtn, $"Menu Key: {toggleKey}");
+
+            keyBtn.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                waitingForKey = true;
+                keybindText.text = "Press any key...";
+            });
+
+            menuObject.SetActive(false);
+        }
+
+        private GameObject CreateButton(GameObject parent, Vector2 pos)
+        {
+            GameObject btnObj = new GameObject("Button");
+            btnObj.transform.SetParent(parent.transform, false);
+
+            var btn = btnObj.AddComponent<Button>();
+            var img = btnObj.AddComponent<Image>();
+            img.color = Color.green;
+
+            RectTransform rt = btnObj.GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(220, 40);
+            rt.anchoredPosition = pos;
+
+            return btnObj;
+        }
+
+        private Text CreateText(GameObject parent, string textValue)
+        {
+            GameObject txtObj = new GameObject("Text");
+            txtObj.transform.SetParent(parent.transform, false);
+
+            var text = txtObj.AddComponent<Text>();
+            text.text = textValue;
+            text.alignment = TextAnchor.MiddleCenter;
+            text.color = Color.black;
+            text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+
+            RectTransform rt = text.GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(220, 40);
+
+            return text;
+        }
+
+        private void UpdateButtonText()
+        {
+            if (speedText != null)
+                speedText.text = speedEnabled ? "Speed: ON" : "Speed: OFF";
+
+            if (keybindText != null)
+                keybindText.text = $"Key: {toggleKey}";
         }
     }
 }
